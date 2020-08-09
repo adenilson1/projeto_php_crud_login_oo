37902abec97113c10317f3a1dc64dce5f81a2830
Adenilson Lima Diniz <39056045+adenilson1@users.noreply.github.com>
2020-08-09 20:44:35 -0300

    Add files via upload

--- phpoo.sql
+++ phpoo.sql
@@ -1,15 +1,13 @@
-CREATE DATABASE  IF NOT EXISTS `phpoo` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `phpoo`;
--- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: phpoo
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.10-MariaDB
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,14 +21,14 @@ USE `phpoo`;
 
 DROP TABLE IF EXISTS `contatos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contatos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `cargo` varchar(150) NOT NULL,
-  `email` varchar(150) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `cargo` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +37,7 @@ CREATE TABLE `contatos` (
 
 LOCK TABLES `contatos` WRITE;
 /*!40000 ALTER TABLE `contatos` DISABLE KEYS */;
-INSERT INTO `contatos` VALUES (3,'adenilson1','Sem cargo','adenilson@hotmail.com'),(9,'teste','Gerente','teste@hotmail.com'),(10,'teste1','Diretor','teste@hotmail.com'),(27,'ad','Diretor','adenilson@hotmail.com'),(30,'adenilson','Secretario','a@hotmail.com'),(25,'adenilson','Diretor','a@hotmail.com'),(26,'adenilson','Secretario','a@hotmail.com'),(28,'adenilson','Gerente','a@hotmail.com'),(29,'adenilson','Secretario','adenilson@hotmail.com'),(35,'ad','Diretor','a@hotmail.com'),(34,'adenilson','Gerente','a@hotmail.com'),(39,'adenilson','Secretario','a@hotmail.com');
+INSERT INTO `contatos` VALUES (1,'John Doe','Funcionario','johndoe@example.com'),(2,'David Deacon','Gerente','daviddeacon@example.com'),(3,'Sam White','Diretor','samwhite@example.com'),(4,'Colin Chaplin','Sem cargo','colinchaplin@example.com'),(5,'Ricky Waltz','Coordenador','rickywaltz@example.com'),(6,'Arnold Hall','Supervisor','arnoldhall@example.com'),(7,'Toni Adams','Coordenador','alvah1981@example.com'),(8,'Donald Perr','Diretor','donald1983@example.com'),(9,'Joe McKinney\'','Secretario','johndoe@example.com'),(10,'Angela Horst','Diretor','angela1977@example.co');
 /*!40000 ALTER TABLE `contatos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,14 +47,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `senha` varchar(150) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +63,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (3,'admin','admin@gmail.com','202cb962ac59075b964b07152d234b70'),(2,'adenilson12','adenilson@hotmail.com','934b535800b1cba8f96a5d72f72f1611'),(4,'teste','teste@hotmail.com','202cb962ac59075b964b07152d234b70'),(5,'ad','a@hotmail.com','202cb962ac59075b964b07152d234b70');
+INSERT INTO `usuarios` VALUES (1,'admin','admin@hotmail.com','123'),(2,'ad','ad@gmail.com','111'),(3,'visita','visita@visita.com','12345');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-06 14:42:55
+-- Dump completed on 2020-08-09 20:41:20
